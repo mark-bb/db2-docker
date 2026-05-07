9f3c288c3472dd80726b96d85573a871d6389e40
Mark Barinstein <mark.barinstein@gmail.com>
2026-05-07 17:53:00 +0300

    Initial version

--- rebuild-nr.sh
+++ rebuild-nr.sh
@@ -5,12 +5,34 @@
 #
 
 usage() {
-  printf "Usage:\n$0 [os-image]\n" >&2
-  exit 1
+  echo -e "Usage example: \n$0 \n\
+          [-b | --base-image] base-image-name - like ubuntu:22.04 \n\
+          " >&2; exit 1;
 }
 
-# IMAGE_BASE=${1:-"redhat/ubi9"}
-IMAGE_BASE=${1:-"ubuntu:22.04"}
+DIR="$(cd "$(dirname "$0")" && pwd -P)"
+# read the options
+TEMP=$(getopt -o hb: --long help,base-image: -n "$0" -- "$@")
+[ $? -ne 0 ] && { echo "Terminating..." >&2; exit 1; }
+
+# Just for test
+#echo "$TEMP"
+eval set -- "$TEMP"
+
+# extract options and their arguments into variables.
+while true ; do
+    case "$1" in
+        -b|--base-image)
+            IMAGE_BASE="$2"; shift 2;;
+        --) shift; break;;
+        -h|--help) usage; exit 1;;
+        *)
+            echo "Internal error!" >&2; exit 1;;
+    esac
+done
+
+# : ${IMAGE_BASE="redhat/ubi9"}
+: ${IMAGE_BASE="ubuntu:22.04"}
 
 IMAGE_SUFFIX="unknown"
 for img in ubuntu redhat suse amazon; do
@@ -21,7 +43,6 @@ for img in ubuntu redhat suse amazon; do
 done
 IMAGE=db2/db2-nr-${IMAGE_SUFFIX?}
 CONT=db2inst1-nr
-DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
 docker stop ${CONT?}
 docker rm -f ${CONT?}
